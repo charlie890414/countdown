3a64d72f2535455cb77dc95ef9e3cfe1c8a9e748
charlie890414 <charlie890414@gmail.com>
2017-03-19 15:02:50 +0800

    change jade to ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CountDown' });
+  res.render('home');
 });
 router.get('/time', function(req, res, next) {
   res.send([2017,4,1]);
